961cecac2faf34decef333621845cdcf70b36f59
walshj19 <walshj19@tcd.ie>
2016-07-18 18:40:15 +0100

    Initial blank project commit

---
+++ app/src/main/java/ie/jameswalshirl/demo/WelcomePage.java
@@ -0,0 +1,13 @@
+package ie.jameswalshirl.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+    }
+}
