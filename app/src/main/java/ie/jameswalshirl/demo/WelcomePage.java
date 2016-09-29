7f9c0f3b07dc6f6dcaf334479ae73edc1b2be338
James Walsh <walshj19@tcd.ie>
2016-09-29 21:25:39 +0100

    Windows commit

--- app/src/main/java/ie/jameswalshirl/demo/WelcomePage.java
+++ app/src/main/java/ie/jameswalshirl/demo/WelcomePage.java
@@ -1,9 +1,9 @@
 package ie.jameswalshirl.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class WelcomePage extends AppCompatActivity {
+public class WelcomePage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
